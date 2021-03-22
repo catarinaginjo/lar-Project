4df9bed7b7551526a97f6d29d9ae39125c594e30
catarinaginjo <36921@ufp.edu.pt>
2021-03-22 17:24:45 +0000

    Installed API on Laravel

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -16,49 +16,53 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/inicio', function () {
-    return view('welcome');
-});
-Route::get('/inicio/utentes', function () {
-    return view('utentes.lista_utentes');
-});
-Route::get('/inicio/auxiliares', function () {
-    return view('funcionarios.auxiliares');
-});
-Route::get('/inicio/cozinheiros', function () {
-    return view('funcionarios.cozinheiros');
-});
-Route::get('/inicio/recados', function () {
-    return view('info.recados');
-});
 
-Route::get('/inicio/recados/id', function () {
-    return view('info.ver_recado');
-});
-Route::get('/inicio/contactos', function () {
-    return view('info.contactos');
-});
-Route::get('/inicio/ementa', function () {
-    return view('ementa');
-});
-Route::get('/inicio/perfil', function () {
-    return view('perfil');
-});
-Route::get('/inicio/stock-alimentos', function () {
-    return view('stock.alimentos');
-});
-Route::get('/inicio/stock-produtos', function () {
-    return view('stock.produtos');
-});
+Route::middleware('auth:api')->group(function () {
+    
+    Route::get('/inicio', function () {
+        return view('welcome');
+    });
+    Route::get('/inicio/utentes', function () {
+        return view('utentes.lista_utentes');
+    });
+    Route::get('/inicio/auxiliares', function () {
+        return view('funcionarios.auxiliares');
+    });
+    Route::get('/inicio/cozinheiros', function () {
+        return view('funcionarios.cozinheiros');
+    });
+    Route::get('/inicio/recados', function () {
+        return view('info.recados');
+    });
 
-Route::get('/inicio/controlo_mp', function () {
-    return view('stock.controlomp');
-});
-Route::get('/inicio/create_utente', function () {
-    return view('utentes.create_utente');
-});
+    Route::get('/inicio/recados/id', function () {
+        return view('info.ver_recado');
+    });
+    Route::get('/inicio/contactos', function () {
+        return view('info.contactos');
+    });
+    Route::get('/inicio/ementa', function () {
+        return view('ementa');
+    });
+    Route::get('/inicio/perfil', function () {
+        return view('perfil');
+    });
+    Route::get('/inicio/stock-alimentos', function () {
+        return view('stock.alimentos');
+    });
+    Route::get('/inicio/stock-produtos', function () {
+        return view('stock.produtos');
+    });
+
+    Route::get('/inicio/controlo_mp', function () {
+        return view('stock.controlomp');
+    });
+    Route::get('/inicio/create_utente', function () {
+        return view('utentes.create_utente');
+    });
 
 
-Route::get('/inicio/utente', function () {
-    return view('utentes.show_utente');
+    Route::get('/inicio/utente', function () {
+        return view('utentes.show_utente');
+    });
 });
